279200e6472cf964e94ef94a448aada32a85a1ac
Thithirat <thithirat.chsw@gmail.com>
2023-09-08 00:41:25 +0700

    refactor: add test case

--- src/Logic/BonusFilterArray/BonusFilterArray.test.ts
+++ src/Logic/BonusFilterArray/BonusFilterArray.test.ts
@@ -1,8 +1,13 @@
 import filterArray from "./BonusFilterArray";
 
-describe("pinCodeValidation", () => {
+describe("filterArray", () => {
   it("should return [2, 4, 6] because 2, 4, and 6 are present in both arrays", () => {
     const response = filterArray([1, 2, 3, 4, 5, 6], [2, 4, 6, 8]);
     expect(response).toEqual([2, 4, 6]);
+  });
+
+  it("should return empty array", () => {
+    const response = filterArray([1, 3, 5], [2, 4, 6, 8]);
+    expect(response).toEqual([]);
   });
 });
